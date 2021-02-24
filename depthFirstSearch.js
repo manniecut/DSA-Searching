0d8f8a6d640e32995affe9cd5332681c7720dad4
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-02-24 00:35:59 -0500

    Update depthFirstSearch.js

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -1,3 +1,5 @@
+const Queue = require("./queue");
+
 class BinarySearchTree {
     constructor(key = null, value = null, parent = null) {
         this.key = key;
@@ -18,8 +20,32 @@ class BinarySearchTree {
             values = this.right.dfs(values);    // recursive function on node
         }   // this makes sense because if left populates first, 
             // going right should dead end with left leaf nodes
-            
+
         return values
+    }
+
+    bfs(tree, values = []) {    //bfs function taking in tree, and values - default empty
+
+        const queue = new Queue();  // create a queue
+        const node = tree.root;     // start with root node
+        queue.enqueue(node);    // add current node to queue
+
+        while (queue.length) {      // as long as there is a queue
+            const node = queue.dequeue();   // take node from queue
+            values.push(node.value);    // add value to the array
+
+                // after that is done, the same node's childs if any, are added to queue, triggering the queue loop to continue
+            if (node.left) {    // add left child to queue
+                queue.enqueue(node.left);
+            }
+            if (node.right) {   // add right child to queue
+                queue.enqueue(node.right);
+            }
+        }
+
+
+        // when the queue is empty, and all of the children have been added to array, return result
+        return values;
     }
 
 }
